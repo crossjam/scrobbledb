aff2064d28a39a762e9d3702f8e1a7c78a175b0a
Brian M. Dennis <bmd+github@crossjam.net>
2025-11-24 22:51:47 -0500

    Add basic test for ScrobbleDataAdapter

---
+++ tests/test_data_adapter.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestScrobbleDataAdapter(unittest.TestCase):
+    def test_basic_functionality(self):
+        # Example test for ScrobbleDataAdapter
+        self.assertTrue(True, "This test should always pass")
+
+if __name__ == '__main__':
+    unittest.main()
